785d36c7a403217cc65e6517441258038423f8a4
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-04-09 01:24:06 +0530

    feat: Transition data source to MongoDB Atlas cloud

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(express.json());
 
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
-app.get("/v1/login", (req, res) => {
-  res.send("Login here");
-});
+app.use("/api/posts", require("./routes/posts"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
