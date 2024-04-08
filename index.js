d89ff71fdd44a895eb2a8ea78095f4f0ab4efdf1
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-04-09 01:49:53 +0530

    add demo api endpoint for testing

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(express.json());
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
